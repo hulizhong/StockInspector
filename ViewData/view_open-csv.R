6f8b6e3bafe6cd3fbefcfaf100c204789137e71e
rabinhu <hulizhong@yeah.net>
2017-07-22 18:27:33 +0800

    Add abline with summary in viewdata.

--- ViewData/view_open-csv.R
+++ ViewData/view_open-csv.R
@@ -2,6 +2,12 @@
 data=read.csv("f:/volumn.csv", header=TRUE)
 p=data$open
 
+p_sum=summary(p)
+p_ref=as.vector(p_sum)
+cat(p_sum[2], "  ", p_sum[3], "  ", p_sum[5], "\n")
+
 plot(p, type='o', col='blue')
-abline(h=median(p), col='blue')
+abline(h=p_ref[2], col='blue1')
+abline(h=p_ref[3], col='blue2')
+abline(h=p_ref[5], col='blue3')
 
